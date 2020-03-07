c66d3643bf2c269d71b8d46527ce21b1dc615df4
Pablo Balbi <pablo.l.balbi@gmail.com>
2020-03-06 22:06:22 -0800

    WIP 2

---
+++ EnvironmentVariableWriterTask.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Microsoft.Build.Utilities;
+
+namespace AnalyzeDotNetProject
+{
+    public class EnvironmentVariableWriterTask : Task
+    {
+        public EnvironmentVariableWriterTask()
+        {
+        }
+
+        public override bool Execute()
+        {
+            Log.LogMessage("About to print all environt variables in MSBuils scopre");
+            foreach(string envVarName in Environment.GetEnvironmentVariables().Keys)
+            { 
+				Log.LogMessage($"{envVarName}: {Environment.GetEnvironmentVariable(envVarName)}");
+			}
+            return true;
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -5,6 +5,7 @@ using CommandLine;
 using Microsoft.Build.Evaluation;
 using System.Collections.Generic;
 using Microsoft.Build.Execution;
+using Microsoft.Build.Logging;
 
 namespace AnalyzeDotNetProject
 {
@@ -23,16 +24,38 @@ namespace AnalyzeDotNetProject
 
         public static void test(string projectFilePath)
         {
+
+            Dictionary<string, string> environmentVarsToSet = new Dictionary<string, string>
+            {
+                {"MSBuildExtensionsPath", "/usr/local/share/dotnet/sdk/3.1.102/" },
+                {"MSBuildLoadMicrosoftTargetsReadOnly", "true" },
+                {"MSBuildSDKsPath", "/usr/local/share/dotnet/sdk/3.1.102/Sdks" },
+            };
+
+            foreach (var envVarName in environmentVarsToSet.Keys)
+            {
+                Environment.SetEnvironmentVariable(envVarName, environmentVarsToSet[envVarName]);
+			}
+
             ProjectCollection pc = new ProjectCollection();
             Dictionary<string, string> GlobalProperty = new Dictionary<string, string>
             {
                 { "Configuration", "Debug" },
                 { "Platform", "x86" },
                 { "TargetFramework", "netcoreapp3.1" },
-                { "UseLegacySdkResolver", "true" }
+                { "UseLegacySdkResolver", "true" },
+                {"MSBuildExtensionsPath", "/usr/local/share/dotnet/sdk/3.1.102/" },
+                {"MSBuildLoadMicrosoftTargetsReadOnly", "true" },
+                {"MSBuildSDKsPath", "/usr/local/share/dotnet/sdk/3.1.102/Sdks" },
             };
-            BuildRequestData BuidlRequest = new BuildRequestData(projectFilePath, GlobalProperty, null, new string[] { "GenerateRestoreGraphFile" }, null);
-            BuildResult buildResult = BuildManager.DefaultBuildManager.Build(new BuildParameters(pc), BuidlRequest);
+            BuildRequestData buildRequest = new BuildRequestData(projectFilePath, GlobalProperty, null, new string[] { "GenerateRestoreGraphFile" }, null);
+            BuildManager.DefaultBuildManager.BeginBuild(new BuildParameters
+            {
+                // NodeExeLocation = "/usr/local/share/dotnet/sdk/3.1.102/Sdk/MSBuild.dll",
+                Loggers = new[] { new ConsoleLogger(Microsoft.Build.Framework.LoggerVerbosity.Detailed) },
+            });
+            var buildSubmission = BuildManager.DefaultBuildManager.PendBuildRequest(buildRequest);
+            BuildResult buildResult = buildSubmission.Execute();
         }
 
         private static void RunFromOptions(Options options)
@@ -41,6 +64,7 @@ namespace AnalyzeDotNetProject
             var dependencyGraph = dependencyGraphService.GenerateDependencyGraph(options.ProjectPath);
 
             test(options.ProjectPath);
+            Environment.Exit(1);
 
             foreach(var project in dependencyGraph.Projects.Where(p => p.RestoreMetadata.ProjectStyle == ProjectStyle.PackageReference))
             {
